701397fd9f2a99f8d7819be79e486047306a00ec
svetoslavche <99680211+svetoslavche@users.noreply.github.com>
2022-08-01 17:11:18 +0300

    4. Token Balances

--- contracts/Token.sol
+++ contracts/Token.sol
@@ -9,12 +9,15 @@ contract Token {
 	uint256 public decimals = 18; // Leave the decimals hardcoded because having 18 is universal for ERC-20
 	uint256 public totalSupply; 
 
-	constructor(string memory _name, string memory _symbol, uint256 _totalSupply) {
+	mapping (address => uint256) public balanceOf; 	// Track balances
+	
+	// Send tokens
+
+	constructor(string memory _name, string memory _symbol, uint256 _totalSupply) { // Make the token configurable easily (name,symbol etc.) so that we can create multiple tokens
 		name = _name;
 		symbol = _symbol;
 		totalSupply = _totalSupply * (10**decimals); // 1,000,000 x 10^18
-
-
-	}  // make the token configurable easily (name,symbol etc.) so that we can create multiple tokens
+		balanceOf[msg.sender] = totalSupply; // Write info to the mapping - Assign all the tokens to the address that has deployed the smart contract
+	}  
 
 }
--- test/Token.js
+++ test/Token.js
@@ -1,17 +1,20 @@
-const { expect } = require('chai'); // pulling the expect function from Chai library from Mocha in Hardhat
-const { ethers } = require('hardhat'); // pulling Ethers.js from Hardhat and saving it to a variable
+const { expect } = require('chai'); // Pulling the expect function from Chai library from Mocha in Hardhat
+const { ethers } = require('hardhat'); // Pulling Ethers.js from Hardhat and saving it to a variable
 
-const tokens = (n) => { // we want this to be a dynamic function that can convert any number we want to
-  return ethers.utils.parseUnits(n.toString(), 'ether') // convert 1000000 to Wei
+const tokens = (n) => { // We want this to be a dynamic function that can convert any number we want to
+  return ethers.utils.parseUnits(n.toString(), 'ether') // Convert 1000000 to Wei
 }
 
 describe('Token', () => {
-  let token     
+  let token, accounts, deployer 
 
-  beforeEach(async () => { // code that gets executed before each one of the examples below
+  beforeEach(async () => { // Code that gets executed before each one of the examples below
     // Fetch token from Blockchain
-    const Token = await ethers.getContractFactory('Token') // getting the factory
-    token = await Token.deploy('Dapp University', 'DAPP', '1000000') // Deploying it    
+    const Token = await ethers.getContractFactory('Token') // Getting the factory
+    token = await Token.deploy('Dapp University', 'DAPP', '1000000') // Deploying it
+    
+    accounts = await ethers.getSigners()
+    deployer = accounts[0]
   })
 
   describe('Deployment', () => { // Tests go inside this separate block 
@@ -33,10 +36,12 @@ describe('Token', () => {
     })
 
     it('has correct total supply', async () => {
-      expect(await token.totalSupply()).to.equal(totalSupply) // Check that decimals are correct
+      expect(await token.totalSupply()).to.equal(totalSupply) // Check that total supply is correct
     })
-
+ 
+    it('assigns total supply to deployer', async () => {
+      expect(await token.balanceOf(deployer.address)).to.equal(totalSupply) // Check that total supply is correct
     })
-
-
+  })
 })
+
