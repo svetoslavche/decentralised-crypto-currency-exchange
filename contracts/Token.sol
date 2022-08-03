e4a5d1138c109800e0e831cc786ad29b3d6c67b4
svetoslavche <99680211+svetoslavche@users.noreply.github.com>
2022-08-03 12:22:57 +0300

    5. Token transfers

--- contracts/Token.sol
+++ contracts/Token.sol
@@ -10,14 +10,36 @@ contract Token {
 	uint256 public totalSupply; 
 
 	mapping (address => uint256) public balanceOf; 	// Track balances
-	
-	// Send tokens
 
-	constructor(string memory _name, string memory _symbol, uint256 _totalSupply) { // Make the token easily configurable (name,symbol etc.) so that we can create multiple tokens
+	event Transfer(
+		address indexed from, 
+		address indexed to, 
+		uint256 value
+	);
+
+	constructor(
+		string memory _name, 
+		string memory _symbol, 
+		uint256 _totalSupply
+	) { // Make the token easily configurable (name,symbol etc.) so that we can create multiple tokens
 		name = _name;
 		symbol = _symbol;
 		totalSupply = _totalSupply * (10**decimals); // 1,000,000 x 10^18
 		balanceOf[msg.sender] = totalSupply; // Write info to the mapping - Assign all the tokens to the address that has deployed the smart contract
 	}  
 
+	function transfer(address _to, uint256 _value) // Send tokens
+		public 
+		returns (bool success) 
+	{ 	
+		require(balanceOf[msg.sender] >= _value); // Require that sender has enough tokens to spend
+		require(_to != address(0)); // and it is the correct address
+
+		balanceOf[msg.sender] = balanceOf[msg.sender] - _value; // Deduct tokens from spender
+		balanceOf[_to] = balanceOf[_to] + _value; // Credit tokens to receiver
+
+		emit Transfer(msg.sender, _to, _value); // Emit event
+
+		return true;
+	} 
 }
--- test/Token.js
+++ test/Token.js
@@ -6,13 +6,14 @@ const tokens = (n) => { // We want this to be a dynamic function that can conver
 }
 
 describe('Token', () => { // Tests go inside this separate block 
-  let token, accounts, deployer // Declaring variables here so their scope is accessible inside every single function below
+  let token, accounts, deployer, receiver // Declaring variables here so their scope is accessible inside every single function below
 
   beforeEach(async () => { // Code that gets executed before each one of the examples below. Speeds up the testing time and keeps the code clean
     const Token = await ethers.getContractFactory('Token') // Fetch token from blockchain
     token = await Token.deploy('Dapp University', 'DAPP', '1000000') // Assign value to the token
-    accounts = await ethers.getSigners() // Pull the getSigners() function from ethers to fetch the accounts
-    deployer = accounts[0] // Assign the number 0 address from the hardhat node
+    accounts = await ethers.getSigners() // Pull the getSigners() function from ethers to fetch the accounts from Hardhat node
+    deployer = accounts[0] // Assign the number 0 address from Hardhat node
+    receiver = accounts[1] // Assign the number 1 address to a receiver
   })
 
   describe('Deployment', () => { // Tests go inside this separate block 
@@ -41,5 +42,44 @@ describe('Token', () => { // Tests go inside this separate block
       expect(await token.balanceOf(deployer.address)).to.equal(totalSupply) // Check that total supply is assigned to the deployer's address 
     })
   })
-})
 
+  describe('Sending Tokens', () => {
+    let amount, transaction, result
+
+    describe('Success', () => {
+
+      beforeEach(async () => { // This beforeEach() sends tokens
+        amount = tokens(100) 
+        transaction = await token.connect(deployer).transfer(receiver.address, amount) 
+        result = await transaction.wait()      
+      })
+
+      it('transfers token balances', async () => { // Check that tokens were transfered (balance changed)
+        expect(await token.balanceOf(deployer.address)).to.equal(tokens(999900))  
+        expect(await token.balanceOf(receiver.address)).to.equal(amount)
+      }) 
+
+      it('emits a Transfer event', async () => {
+        const event = result.events[0]
+        expect(event.event).to.equal('Transfer')
+
+        const args = event.args // Reading the event args from console.log which we've removed from this code
+        expect(args.from).to.equal(deployer.address)
+        expect(args.to).to.equal(receiver.address)
+        expect(args.value).to.equal(amount)
+      })
+    })
+
+    describe('Failure', () => {
+      it('rejects insufficient balances', async () => { // Transfer more tokens than deployer has - 100M
+        const invalidAmount = tokens(100000000) 
+        await expect(token.connect(deployer).transfer(receiver.address, invalidAmount)).to.be.reverted
+      })
+
+      it('rejects invalid recepient', async () => { // Make sure you can't send tokens to random address
+        const amount = tokens(100) 
+        await expect(token.connect(deployer).transfer('0x00000000000000', amount)).to.be.reverted
+      })
+    })
+  })
+})
