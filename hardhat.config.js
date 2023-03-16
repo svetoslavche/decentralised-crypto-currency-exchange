c4fd25062007978f345b100e26a361c3ca6308da
svetoslavche <99680211+svetoslavche@users.noreply.github.com>
2023-03-16 23:51:34 +0200

    32.6 Test Network Update 2

--- hardhat.config.js
+++ hardhat.config.js
@@ -22,8 +22,8 @@ module.exports = {
   solidity: "0.8.9",
   networks: {
     localhost: {},
-    sepolia: {
-      url: `https://sepolia.infura.io/v3/${process.env.INFURA_API_KEY}`,
+    goerli: {
+      url: `https://goerli.infura.io/v3/${process.env.INFURA_API_KEY}`,
       accounts: privateKeys.split(','),
     }
   },
